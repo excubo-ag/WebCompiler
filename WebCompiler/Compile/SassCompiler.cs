d6efe97371e7c16d6358a63cdaa66b1ccaa79a09
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2021-07-30 16:33:26 +0200

    [deploy] fixed scope

--- WebCompiler/Compile/SassCompiler.cs
+++ WebCompiler/Compile/SassCompiler.cs
@@ -67,9 +67,10 @@ namespace WebCompiler.Compile
                 {
                     options.IncludePaths.Add(settings.IncludePath);
                 }
+                CompilationResult? compile_result;
                 using (var compiler = new DartSassHost.SassCompiler(new ChakraCoreJsEngineFactory()))
                 {
-                    var compile_result = compiler.CompileFile(file, tmp_output_file, file, options);
+                    compile_result = compiler.CompileFile(file, tmp_output_file, file, options);
                 }
                 var replaced = ReplaceIfNewer(output_file, compile_result.CompiledContent);
                 if (!autoprefix_settings.Enabled)
